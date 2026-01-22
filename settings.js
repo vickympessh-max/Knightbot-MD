7b4c92dd5ef0f5696513a29ad07afc1c6ec0d885
vickympessh-max <vickympessh@gmail.com>
2026-01-22 13:50:15 +0300

    Update bot author and owner settings

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: 'victor‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Vicky', // Your name
+  ownerNumber: '254796422647', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
