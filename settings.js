3be56cbf56436fb98a3e5a4e984eb8eacae37dc3
vickympessh-max <vickympessh@gmail.com>
2026-03-08 09:37:41 +0300

    Update bot name and pack name to 'Vickies AI'

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Vickies AI',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Vickies AI",
   botOwner: 'Professor', // Your name
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
